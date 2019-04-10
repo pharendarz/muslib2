80a11a8691f00c40bddb35427cfa21c737257812
hendels <p.harendarz@gmail.com>
2019-04-10 20:04:45 +0200

    added disk / folder reader for .FLAC file

---
+++ flac_management/flacMgt.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+const path = require('path');
+
+function flatten(lists) {
+    // console.log('lists = ', i, lists)
+    return lists.reduce((a, b) => a.concat(b), []);
+}
+//let i = 0;  
+function getDirectories(srcpath) {
+    const readdir = fs.readdirSync(srcpath);
+    //console.log(readdir);
+    const pathArray = readdir.map(file => path.join(srcpath, file));
+    //console.log(pathArray);
+    const filteredArray = pathArray.filter(path => fs.statSync(path).isDirectory());
+    //console.log(filteredArray);
+    return filteredArray;
+    // return fs.readdirSync(srcpath)
+    //     .map(file => {return path.join(srcpath, file)})
+    //     .filter(path => fs.statSync(path).isDirectory());
+}
+function findSpecificFileFormat(startPath, filter){
+    if (!fs.existsSync(startPath)){
+        console.log("no dir", startPath);
+        return;
+    }
+    let pathsWithFilter = [];
+    let files = fs.readdirSync(startPath);
+    //console.log('start path file = ', startPath);
+    //console.log('files = ', files);
+    for(let i=0; i<files.length; i++){
+        //console.log('files array = ', files[i]);
+        if (files[i] !== undefined){
+            let filename = path.join(startPath, files[i]);
+            //console.log('filenamePath = ', filename);
+
+            let stat = fs.lstatSync(filename);
+            if (stat.isDirectory()){
+                findSpecificFileFormat(filename, filter); //recurse
+            }
+            else if (filename.indexOf(filter)>=0){
+                //console.log('--found ', filename);
+                pathsWithFilter.push(filename);
+            }
+        }
+    }
+    if (pathsWithFilter.length === 0) 
+        return null;
+    else
+        return pathsWithFilter;
+}
+function getDirectoriesRecursive(srcpath) {
+    
+    const dirs = getDirectories(srcpath);
+    //console.log('iterate = ', i+=1, dirs);
+    const recursive = dirs.map(path => {
+        return getDirectoriesRecursive(path)
+    });
+    //console.log('recursive', recursive);
+    return [srcpath, ...flatten(recursive)];
+}
+
+exports.readDrive = () => {
+    const srcpath = "/home/ouce/Desktop/muslibJS/testflac/";
+    const allPaths = getDirectoriesRecursive(srcpath);
+    //console.log('read drive mister', allPaths);
+    let fileList = []
+    allPaths.map(path => {
+        //console.log(path.toString());
+        if (path !== undefined){
+            let filename = findSpecificFileFormat(path.toString(), '.FLAC');
+            //console.log(filename);
+            if(filename !== null) fileList.push(filename);
+        }
+    });
+    fileList = fileList.reduce((a, b) => a.concat(b), []);
+    console.log(fileList);
+}
--- routes/getRoutes.js
+++ routes/getRoutes.js
@@ -1,3 +1,5 @@
+const flacMgt = require('../flac_management/flacMgt');
+
 module.exports = app => {
     app.get('/readflac', (req, res) => {
         require('../flac_management/flacRead.js');
@@ -6,5 +8,9 @@ module.exports = app => {
     app.get('/writeflac', (req, res) => {
         require('../flac_management/flacWrite.js');
         res.send({server: 'writing FLAC'});
+    });
+    app.get('/readdrive', (req, res) => {
+        flacMgt.readDrive();
+        res.send({server: 'reading drive'});
     })
 }
